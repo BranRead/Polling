f0549577a44e7c9ab3b71ca37340f7a503c8ad3b
BranRead <branrtread@gmail.com>
2023-04-09 13:51:00 -0300

    I made the login system work!
    Now to add features to each account

--- main.c
+++ main.c
@@ -6,9 +6,11 @@
 //user and admins
 
 void userLogin();
-void staffLogin();
+void staffLogin(int useCase, char usernameFile[50], char passwordFile[50], FILE *username, FILE *password);
 
-void loggedIn();
+void loggedInVoter();
+void loggedInAdmin();
+void loggedInOfficer();
 
 #define FILENAME_SIZE 1024
 #define MAX_LINE 2048
@@ -16,46 +18,53 @@ void loggedIn();
 char filename[FILENAME_SIZE];
 char buffer[MAX_LINE];
 
-FILE *usernameData;
-FILE *passwordData;
+FILE *usernameData_Voter;
+FILE *passwordData_Voter;
+
+FILE *usernameData_Admin;
+FILE *passwordData_Admin;
+
+FILE *usernameData_Officer;
+FILE *passwordData_Officer;
 
 
 
 int main() {
     int useCase;
 
-    usernameData = fopen("username_column.txt", "w");
-    fprintf(usernameData, "JohnUser\n");
-    fclose(usernameData);
+    //Voter
+    usernameData_Voter = fopen("usernameVoter_column.txt", "w");
+    fprintf(usernameData_Voter, "JohnUser\n");
+    fclose(usernameData_Voter);
 
-    passwordData = fopen("password_column.txt", "w");
-    fprintf(passwordData, "Passw0rd\n");
-    fclose(passwordData);
+    passwordData_Voter = fopen("passwordVoter_column.txt", "w");
+    fprintf(passwordData_Voter, "Passw0rd\n");
+    fclose(passwordData_Voter);
 
-//    char enteredName[] = "JohnUser\n";
+    //Admin
+    usernameData_Admin = fopen("usernameAdmin_column.txt", "w");
+    fprintf(usernameData_Admin, "JohnAdmin\n");
+    fclose(usernameData_Admin);
 
-//
-//     "JohnUser",
-//    "Passw0rd",
-//    "JaneUser",
-//    "PaSsWoRd",
-//
+    passwordData_Admin = fopen("passwordAdmin_column.txt", "w");
+    fprintf(passwordData_Admin, "Passw0rd\n");
+    fclose(passwordData_Admin);
 
-//   "JohnAdmin";
-//    "PASSw0rd";
-//
-//
-//    "JaneOfficer";
-//    "icountvotes";
-//
-//
+    //Polling Officer
+    usernameData_Officer = fopen("usernameOfficer_column.txt", "w");
+    fprintf(usernameData_Officer, "JohnOff\n");
+    fclose(usernameData_Officer);
+
+    passwordData_Officer = fopen("passwordOfficer_column.txt", "w");
+    fprintf(passwordData_Officer, "Passw0rd\n");
+    fclose(passwordData_Officer);
+
     bool run = true;
-//
+
     void useExp();
     void admExp();
     void polExp();
 
-//
     do{
         printf("Key: User = 1 | Admin = 2 | Polling Officer = 3 | Exit = 0\n");
         printf("What type of user are you?\n");
@@ -74,11 +83,13 @@ int main() {
                     break;
                 case 2:
                     //Admin
-                    staffLogin();
+                    staffLogin(useCase, "usernameAdmin_column.txt", "passwordAdmin_column.txt",
+                               usernameData_Admin, passwordData_Admin);
                     break;
                 case 3:
                     //Polling Officer
-                    staffLogin();
+                    staffLogin(useCase, "usernameOfficer_column.txt", "passwordOfficer_column.txt",
+                               usernameData_Officer, passwordData_Officer);
                     break;
                 case 0:
                     //Quit
@@ -113,28 +124,28 @@ void userLogin() {
             printf("Please enter password: ");
             fgets(password, 21, stdin);
 
-            usernameData = fopen("username_column.txt", "r");
+            usernameData_Voter = fopen("username_column.txt", "r");
             bool keep_reading = true;
             int current_line = 1;
             int readLine = 0;
             do {
-                fgets(buffer, MAX_LINE, usernameData);
-                if(feof(usernameData)){
+                fgets(buffer, MAX_LINE, usernameData_Voter);
+                if(feof(usernameData_Voter)){
                     printf("Not found!\n");
                     keep_reading = false;
                 } else if(strcmp(buffer, username) == 0){
                     printf("Correct username!\n");
                     readLine++;
-                    passwordData = fopen("password_column.txt", "r");
+                    passwordData_Voter = fopen("password_column.txt", "r");
                     do {
-                        fgets(buffer, MAX_LINE, passwordData);
-                        if(feof(passwordData)){
+                        fgets(buffer, MAX_LINE, passwordData_Voter);
+                        if(feof(passwordData_Voter)){
                             printf("Not found!\n");
                             keep_reading = false;
                         } else if(current_line == readLine){
                             if(strcmp(buffer, password) == 0){
                                 printf("Password Matches login name, you are now logged in\n");
-                                loggedIn();
+                                loggedInVoter();
                                 keep_reading = false;
                             } else {
                                 printf("Username/Password Combo incorrect");
@@ -142,13 +153,13 @@ void userLogin() {
                         }
                         current_line++;
                     } while(keep_reading);
-                    fclose(passwordData);
+                    fclose(passwordData_Voter);
                     keep_reading = false;
                 } else {
                     readLine++;
                 }
             } while(keep_reading);
-            fclose(usernameData);
+            fclose(usernameData_Voter);
             break;
         case 2:
             printf("Please register!");
@@ -158,16 +169,67 @@ void userLogin() {
     }
 };
 
-void staffLogin() {
+void staffLogin(int useCase, char usernameFile[50], char passwordFile[50], FILE *usernameVar, FILE *passwordVar) {
     printf("Please enter username: ");
     char username[20];
-    scanf("%s", username);
+    fgets(username, 21, stdin);
     char password[20];
     printf("Please enter password: ");
-    scanf("%s", password);
+    fgets(password, 21, stdin);
+
+    usernameVar = fopen((const char *) usernameFile, "r");
+    bool keep_reading = true;
+    int current_line = 1;
+    int readLine = 0;
+    do {
+        fgets(buffer, MAX_LINE, usernameData_Voter);
+        if(feof(usernameVar)){
+            printf("Not found!\n");
+            keep_reading = false;
+        } else if(strcmp(buffer, username) == 0){
+            printf("Correct username!\n");
+            readLine++;
+            passwordVar = fopen((const char *) passwordFile, "r");
+            do {
+                fgets(buffer, MAX_LINE, passwordVar);
+                if(feof(passwordVar)){
+                    printf("Not found!\n");
+                    keep_reading = false;
+                } else if(current_line == readLine){
+                    if(strcmp(buffer, password) == 0){
+                        printf("Password Matches login name, you are now logged in\n");
+                        if(useCase == 2){
+                            loggedInAdmin();
+                        } else if (useCase == 3){
+                            loggedInOfficer();
+                        } else {
+                            printf("Please report this error to the person responsible for this software."
+                                   " Code:197");
+                        }
+                        keep_reading = false;
+                    } else {
+                        printf("Username/Password Combo incorrect");
+                    }
+                }
+                current_line++;
+            } while(keep_reading);
+            fclose(passwordVar);
+            keep_reading = false;
+        } else {
+            readLine++;
+        }
+    } while(keep_reading);
+    fclose(usernameVar);
 };
 
-void loggedIn(){
-    printf("You are now logged in\n");
+void loggedInVoter(){
+    printf("You are now logged in as user\n");
 };
 
+void loggedInAdmin(){
+    printf("You are now logged in as admin\n");
+};
+
+void loggedInOfficer(){
+    printf("You are now logged in as polling officer\n");
+};
